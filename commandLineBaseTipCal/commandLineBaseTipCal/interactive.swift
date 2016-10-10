1920e49e78a47e23e9fe4172aa68492448ea5645
ctrador <chad.trador@interapthq.com>
2016-10-10 10:27:32 -0400

    tip cal mac OS

---
+++ commandLineBaseTipCal/commandLineBaseTipCal/interactive.swift
@@ -0,0 +1,9 @@
+//
+//  interactive.swift
+//  commandLineBaseTipCal
+//
+//  Created by Chad Trador on 10/10/16.
+//  Copyright © 2016 Chad Trador. All rights reserved.
+//
+
+import Foundation
---
+++ commandLineBaseTipCal/commandLineBaseTipCal/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  commandLineBaseTipCal
+//
+//  Created by Chad Trador on 10/10/16.
+//  Copyright © 2016 Chad Trador. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
